43a102bcbc31408292b8d5babb82686ffc05d32d
sonms <smsms5676@naver.com>
2025-09-22 02:19:37 +0900

    feat/#3 domain - entity, repository, usecase

---
+++ app/src/main/java/com/code/gram/data/dto/AuthResponse.kt
@@ -0,0 +1,23 @@
+package com.code.gram.data.dto
+
+import com.code.gram.domain.entity.LoginEntity
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AuthResponse(
+    @SerialName("status")
+    val status: String,
+    @SerialName("access_token")
+    val accessToken: String = "",
+    @SerialName("refresh_token")
+    val refreshToken: String = "",
+    @SerialName("signup_token")
+    val signupToken: String,
+    @SerialName("token_type")
+    val tokenType: String
+) {
+    fun toDomain(): LoginEntity {
+        return LoginEntity(status, accessToken, refreshToken, signupToken, tokenType)
+    }
+}
---
+++ app/src/main/java/com/code/gram/domain/entity/LoginEntity.kt
@@ -0,0 +1,9 @@
+package com.code.gram.domain.entity
+
+data class LoginEntity(
+    val status: String,
+    val accessToken: String,
+    val refreshToken: String,
+    val signupToken: String,
+    val tokenType: String
+)
