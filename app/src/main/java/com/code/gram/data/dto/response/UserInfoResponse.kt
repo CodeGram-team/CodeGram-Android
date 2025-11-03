7a7235311c3f5d40cff0d3d4d51226db6aa4df8f
sonms <smsms5676@naver.com>
2025-11-03 23:19:14 +0900

    feat: 프로필 정보 DTO 및 Entity 추가
    - 유저 정보를 담는 `UserInfoResponse` DTO 및 `UserInfoEntity` 추가
    - 유저의 게시물 목록을 나타내는 `Post` DTO 및 `PostEntity` 추가
    - `UserInfoResponse` DTO를 `UserInfoEntity`로 변환하는 `toDomain()` 매핑 함수 구현

---
+++ app/src/main/java/com/code/gram/data/dto/response/UserInfoResponse.kt
@@ -0,0 +1,68 @@
+package com.code.gram.data.dto.response
+
+import com.code.gram.domain.entity.profile.PostEntity
+import com.code.gram.domain.entity.profile.UserInfoEntity
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
+
+@Serializable
+data class UserInfoResponse(
+    @SerialName("id")
+    val id: String,
+
+    @SerialName("nickname")
+    val nickname: String,
+
+    @SerialName("username")
+    val username: String,
+
+    @SerialName("profile_image_url")
+    val profileImageUrl: String,
+
+    @SerialName("created_at")
+    val createdAt: String, // 필요시 Instant로 변환 가능
+
+    @SerialName("posts")
+    val posts: List<Post>,
+
+    @SerialName("email")
+    val email: String
+) {
+    fun toDomain(): UserInfoEntity {
+        return UserInfoEntity(
+            id = id,
+            nickname = nickname,
+            username = username,
+            profileImageUrl = profileImageUrl,
+            createdAt = createdAt,
+            posts = posts.map { it.toDomain() },
+            email = email
+        )
+    }
+}
+
+@Serializable
+data class Post(
+    @SerialName("_id")
+    val id: String,
+
+    @SerialName("title")
+    val title: String,
+
+    @SerialName("language")
+    val language: String,
+
+    @SerialName("likesCount")
+    val likesCount: Int,
+
+    @SerialName("createdAt")
+    val createdAt: String
+) {
+    fun toDomain(): PostEntity = PostEntity (
+        id = id,
+        title = title,
+        language = language,
+        likesCount = likesCount,
+        createdAt = createdAt
+    )
+}
---
+++ app/src/main/java/com/code/gram/domain/entity/profile/UserInfoEntity.kt
@@ -0,0 +1,19 @@
+package com.code.gram.domain.entity.profile
+
+data class UserInfoEntity(
+    val id: String,
+    val nickname: String,
+    val username: String,
+    val profileImageUrl: String,
+    val createdAt: String,
+    val posts: List<PostEntity>,
+    val email: String
+)
+
+data class PostEntity(
+    val id: String,
+    val title: String,
+    val language: String,
+    val likesCount: Int,
+    val createdAt: String
+)
